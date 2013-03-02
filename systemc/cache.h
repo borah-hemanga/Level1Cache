24c6c6af0da5d933269352d0ed198f900f8c1efd
Hemanga Borah <hborah@allv21.(none)>
2013-03-02 14:09:01 -0500

    variables corrected for types again

--- systemc/cache.h
+++ systemc/cache.h
@@ -73,144 +73,152 @@ class CacheProject: public Cache
 			SRAM_DATA_READY
 		    };
 
-	struct state_parameter {
-		unsigned long int prm_addr[ports];
-		unsigned long int prm_index[ports];
-		unsigned long int prm_tag[ports];
-		unsigned long int prm_state[ports];
-		bool prm_isstate[ports];
-		bool is_read[ports];
-		bool is_write[ports];
-		bool is_insert[ports];
-		int  next_state[ports];
-	}state;
-
-	
 	static const int block_offset_bits = 6;
 	static const int block_size_bits   = 8 * block_size;
 	static const int tag_size_bits     = 64 - depthLog2 - block_offset_bits;
 	static const int state_size_bits   = 8;
 
+	struct state_parameter {
+		sc_bv<64>            prm_addr[ports];
+		sc_bv<8>             prm_state[ports];
+		sc_bv<depthLog2>     prm_index[ports];
+		sc_bv<tag_size_bits> prm_tag[ports];
+		bool 		     prm_isstate[ports];
+		bool                 is_read[ports];
+		bool                 is_write[ports];
+		bool                 is_insert[ports];
+		int                  next_state[ports];
+	}state_var;
 
+	
 			/* SRAMs */
         SRAM<block_size_bits,depthLog2,ports> data;
         SRAM<tag_size_bits,depthLog2,ports>   tag;
-        SRAM<state_bits,depthLog2,ports>      state;
+        SRAM<state_size_bits,depthLog2,ports>      state;
 
 
 		/* cache controller signals */
-	sc_signal<bool> tag_ena[ports];
-	sc_signal<sc_bv<block_size_bits> > tag_data[ports];
-	sc_signal<sc_uint<depthLog2> > tag_addr[ports];
-	sc_signal<sc_uint<depthLog2> > tag_addr[ports];
-	sc_signal<bool> tag_we[ports];
+	sc_signal<bool>                    tag_ena[ports];
+	sc_signal<sc_bv<block_size_bits> > tag_din[ports];
+	sc_signal<sc_bv<block_size_bits> > tag_dout[ports];
+	sc_signal<sc_uint<depthLog2> >     tag_addr[ports];
+	sc_signal<bool>                    tag_we[ports];
 
-	sc_signal<bool> data_ena[ports];
-	sc_signal<sc_bv<tag_size_bits> > data_data[ports];
-	sc_signal<sc_uint<depthLog2> > data_addr[ports];
-	sc_signal<bool> data_we[ports];
+	sc_signal<bool>                    data_ena[ports];
+	sc_signal<sc_bv<tag_size_bits> >   data_din[ports];
+	sc_signal<sc_bv<tag_size_bits> >   data_dout[ports];
+	sc_signal<sc_uint<depthLog2> >     data_addr[ports];
+	sc_signal<bool>                    data_we[ports];
 	
-	sc_signal<bool> state_ena[ports];
-	sc_signal<sc_bv<state_size_bits > > state_data[ports];
-	sc_signal<sc_uint<depthLog2 > > state_addr[ports];
-	sc_signal<bool > state_we[ports];
+	sc_signal<bool>                     state_ena[ports];
+	sc_signal<sc_bv<state_size_bits > > state_din[ports];
+	sc_signal<sc_bv<state_size_bits > > state_dout[ports];
+	sc_signal<sc_uint<depthLog2 > >     state_addr[ports];
+	sc_signal<bool >                    state_we[ports];
 
-       unsigned short delay_cycle[ports] = 0;
+       unsigned short delay_cycle[ports];
 
        void process(int portno) {
 
-	if (state.next_state[portno] == INIT) {		// common to read, write, insert
+	if (state_var.next_state[portno] == INIT) {		// common to read, write, insert
 
-			state.prm_addr[portno]    = in_addr[portno].read();
-			state.prm_index[portno]   = GET_INDEX(state.prm_addr[portno]);
-			state.prm_tag[portno]     = GET_TAG(state.prm_addr[portno]);
-			state.prm_isstate[portno] = in_update_state[portno].read();			
+			state_var.prm_addr[portno]    = in_addr[portno].read();
+			state_var.prm_index[portno]   = GET_INDEX(state_var.prm_addr[portno]);
+			state_var.prm_tag[portno]     = GET_TAG(state_var.prm_addr[portno]);
+			state_var.prm_isstate[portno] = in_update_state[portno].read();			
 
-			if (state.prm_isstate[portno])
-			    state.prm_state[portno]   = in_new_state[portno].read();
+			if (state_var.prm_isstate[portno])
+			    state_var.prm_state[portno]   = in_new_state[portno].read();
 			else
-			    state.prm_state[portno]   = 0;
+		        	/*throw away the received out state*/
+			    state_var.prm_state[portno]   = 0;
 
 				/* Set TAG_SRAM */
-			tag_addr[portno].write(state.prm_index[portno]);
-			tag_ena[portno]            = true;
+			tag_addr[portno].write(state_var.prm_index[portno]);
+			tag_ena[portno].write(true);
 
 				/* Set STATE_SRAM */
-			state_addr[portno]        = state.prm_index[portno];
-			state_ena[portno]          = true;
+			state_addr[portno].write(state_var.prm_index[portno]);
+			state_ena[portno].write(true);
 
 				/* Set STATE_SRAM */
 			delay_cycle[portno]      = 1; 
-			state.next_state[portno] = TAG_AND_STATE_WAIT;
+			state_var.next_state[portno] = TAG_AND_STATE_WAIT;
 
 			return ;	
 				
 	 }
 	
-	if (state.next_state[portno] == TAG_AND_STATE_WAIT) {
+	if (state_var.next_state[portno] == TAG_AND_STATE_WAIT) {
 
 			delay_cycle[portno] = 1;
-			state.next_state[portno] = TAG_AND_STATE_READY;
+			state_var.next_state[portno] = TAG_AND_STATE_READY;
 			return;
 	 }
 
-	if (state.next_state[portno] == TAG_AND_STATE_READY) {
+	if (state_var.next_state[portno] == TAG_AND_STATE_READY) {
 
-            	    if (state.prm_tag[portno] == tag_data[portno] ) { // TAG MATCH
+            	    if (state_var.prm_tag[portno] == tag_dout[portno].read() ) { // TAG MATCH
 					// Cache Hit
 
-			if(state.isread[portno]) {
+			if(state_var.is_read[portno]) {
 
-				state.prm_state[portno] = state.din[portno];
+				state_var.prm_state[portno] = state_dout[portno].read();
 
 				/*disable SRAMS in the nextcycle*/
 
-				data_addr[portno] = state.prm_index[portno];
-				data_ena[portno]  = true;
+	     		        tag_ena[portno].write(false);
+  	     			state_ena[portno].write(false);
+
+				data_addr[portno].write(state_var.prm_index[portno]);
+				data_ena[portno].write(true);
 
 		        	delay_cycle[portno] = 1;
-				state.next_state[portno] = DATA_SRAM_WAIT;
+				state_var.next_state[portno] = SRAM_DATA_WAIT;
 				return;
 			}
 					/* Dirty Write */
-		        else if(state.iswrite[portno]) {
-				state.prm_state[portno] = state.din[portno];
 
-			        if (state.prm_isstate[portno])     {
-			            state.din[portno] = state.prm_state[portno];
-				    state_addr[portno] = state.prm_index[portno];
-				    state_we[portno]   = true;
+		        else if(state_var.is_write[portno]) {
 
+				state_var.prm_state[portno] = state_dout[portno].read();
+
+			        if (state_var.prm_isstate[portno])     {
+			            state_din[portno].write(state_var.prm_state[portno]);
+				    state_addr[portno].write(state_var.prm_index[portno]);
+				    state_we[portno].write(true);
+				/* We can go ahead with the SRAM Write*/
 				}
 
-				data_addr[portno] = state.prm_index[portno];
-				data.din[portno] = in_data[portno];
+				data_din[portno].write(in_data[portno]);
+				data_addr[portno].write(state_var.prm_index[portno]);
 
-				data_we[portno]   = true;
-				data_ena[portno]  = true;
+				data_we[portno].write(true);
+				data_ena[portno].write(true);
 
 		        	delay_cycle[portno] = 1;
-				state.next_state[portno] = SRAM_DATA_WAIT;
+				state_var.next_state[portno] = SRAM_DATA_WAIT;
 				return;
 			}
 
-			else if(state.isinsert[portno]){
-				state.prm_state[portno] = state.din[portno];
+			else if(state_var.is_insert[portno]){
+					/* Read the evicted block enrty state */
+				out_state[portno].write(state_dout[portno].read());
 
-			        if (state.prm_isstate[portno])     {
-			            state.din[portno] = state.prm_state[portno];
-				    state_addr[portno] = state.prm_index[portno];
-				    state_we[portno]   = true;
+			        if (state_var.prm_isstate[portno])     {
+			            state_din[portno].write(state_var.prm_state[portno]);
+				    state_addr[portno].write(state_var.prm_index[portno]);
+				    state_we[portno].write(true);
 			        }
 		
-				data_addr[portno] = state.prm_index[portno];
-				data.din[portno] = in_data[portno];
+				data_addr[portno].write(state_var.prm_index[portno]);
+				data_din[portno].write(in_data[portno]);
 
-				data_we[portno]   = true;
-				data_ena[portno]  = true;
+				data_we[portno].write(true);
+				data_ena[portno].write(true);
 
 		        	delay_cycle[portno] = 1;
-				state.next_state[portno] = DATA_SRAM_WAIT;
+				state_var.next_state[portno] = SRAM_DATA_WAIT;
 				return;
                         }		
 		
@@ -218,48 +226,63 @@ class CacheProject: public Cache
 			// Cache Miss 
             else {
 				/* Read Miss */
-			if(state[portno].isread) {
+			if(state_var.is_read[portno]) {
 
-				tag_ena[portno] = false;
-				state_ena[portno] = false;
-				out_addr[portno].write(tag_data[portno]);
-		        	out_token[portno].write(state.prm_addr[portno]);
+					/*Disable Bothm SRAM's */
+
+				out_addr[portno].write(tag_dout[portno].read());
+		        	out_token[portno].write(state_var.prm_addr[portno]);
 				out_state[portno].write(0xff);
 				out_ready[portno].write(true);
 				port_available[portno] = true;
-				state[portno].isread   = false;
+
+				tag_ena[portno].write(false);
+				state_ena[portno].write(false);
+				state_var.is_read[portno]  = false;
+
 		        	delay_cycle[portno] = 0;
-				state.next_state[portno] = INIT;
+				state_var.next_state[portno] = INIT;
 				return;
 
 			}     /* Write Miss */
-			else if (state[portno].iswrite) {
+			else if (state_var.is_write[portno]) {
 
-				tag_ena[portno] = false;
-				state_ena[portno] = false;
-		        	out_token[portno].write(state.prm_addr[portno]);
+				/* Out Addr should contain the accessed tag */
+				out_addr[portno].write(tag_dout[portno].read());
+		        	out_token[portno].write(state_var.prm_addr[portno]);
 				out_state[portno].write(0xff);
 				out_ready[portno].write(true);
+
+				tag_ena[portno].write(false);
+				state_ena[portno].write(false);
+
 				port_available[portno] = true;
-				state[portno].isread   = false;
+				state_var.isread[portno]   = false;
+
 		        	delay_cycle[portno] = 0;
-				state.next_state[portno] = INIT;
+				state_var.next_state[portno] = INIT;
 				return;
 
 			}	/* Insert */
-			else if (state[portno].isinsert) {
+			else if (state_var.is_insert[portno]) {
 
-			        tag_data[portno] = state.prm_tag[portno];
-				tag_addr[portno] = state.prm_index[portno];
-				tag_we[portno]   = true;
+
+					/*evict and write the tag */
+			        tag_din[portno].write(state_var.prm_tag[portno]);
+				tag_addr[portno].write(state_var.prm_index[portno]);
+				tag_we[portno].write(true);
 				     /* IS_UPDATE_STATE ALWAYS TRUW FOR IS INSERT */
-			        state.din[portno] = state.prm_state[portno];
-				state_addr[portno] = state.prm_index[portno];
-				state_we[portno]   = true;
-			
-				out_state[portno].write(state.din[portno]);
+			        state_din[portno].write(state_var.prm_state[portno]);
+				state_addr[portno].write(state_var.prm_index[portno]);
+				state_we[portno].write(true);
+
+					/*old tag */
+				state_var.prm_tag[portno].write(tag_dout[portno].read());
+					/*evict the old state */
+				state_var.prm_state[portno].write(state_var.dout[portno].read());
+
 		        	delay_cycle[portno] = 1;
-				state.next_state[portno] = SRAM_DATA_WAIT
+				state_var.next_state[portno] = SRAM_DATA_WAIT;
 				return;
 
                          }		
@@ -269,56 +292,60 @@ class CacheProject: public Cache
 
          }
 
-	 if (state.next_state[portno] == SRAM_DATA_WAIT) {
-
-     	     tag_we[portno]     = false;
-	     tag_ena[portno]    = false;
-  	     state_we[portno]   = false;
-  	     state_ena[portno]  = false;
+	 if (state_var.next_state[portno] == SRAM_DATA_WAIT) {
 
              delay_cycle[portno] = 1;
-	     state.next_state[portno] = SRAM_DATA_READY;
+	     state_var.next_state[portno] = SRAM_DATA_READY;
 	     return;
 	 }
 
-	 if(state.next_state[portno] == SRAM_DATA_READY) {
+	 if(state_var.next_state[portno] == SRAM_DATA_READY) {
+        	
+		/* all the SRAM access time identical */
 
-	         if(state.isread[portno])  {
-					/*Write In Blocks*/
-                		out_data[portno].write(data.din[portno]);
-				data_en[portno] = false;
+     	     	 tag_we[portno].write(false);
+	     	 tag_ena[portno].write(false);
 
-				out_addr[portno].write(state.prm_tag[portno]);
-			        out_token[portno].write(state.prm_addr[portno]);
-				out_state[portno].write(state.prm_state[portno]);
+  	         state_we[portno].write(false);
+  	         state_ena[portno].write(false);
+
+	         if(state_var.isread[portno])  {
+					/*Write In Blocks to the Cache Ouput*/
+                		out_data[portno].write(data_dout[portno].read());
+				data_ena[portno].write(false);
+
+				out_addr[portno].write(state_var.prm_tag[portno]);
+			        out_token[portno].write(state_var.prm_addr[portno]);
+				out_state[portno].write(state_var.prm_state[portno]);
 				out_ready[portno].write(true);
 
 				port_available[portno] = true;
 
 			        delay_cycle[portno] = 0;
-				state.next_state[portno] = INIT;
+				state_var.next_state[portno] = INIT;
 				return;
 	         }			
 				
-	         if((state.iswrite[portno]) || (state.isinsert[portno])) {
+	         if((state_var.is_write[portno]) || (state_var.is_insert[portno])) {
 
+				/* Ouput is Empty */
                 		out_data[portno].write(0);
-				data_we[portno] = false;
-				data_en[portno] = false;
+				data_we[portno].write(false);
+				data_en[portno].write(false);
 
-				out_addr[portno].write(state.prm_tag[portno]);
-			        out_token[portno].write(state.prm_addr[portno]);
-				out_state[portno].write(state.prm_state[portno]);
+				out_addr[portno].write(state_var.prm_tag[portno]);
+			        out_token[portno].write(state_var.prm_addr[portno]);
+				out_state[portno].write(state_var.prm_state[portno]);
 				out_ready[portno].write(true);
 
 				port_available[portno] = true;
 			        delay_cycle[portno] = 0;
-				state.next_state[portno] = INIT;
+				state_var.next_state[portno] = INIT;
 				return;
 	         }			
 
 
-	     }		
+	     }	//SRAM_ACCESS	
 	     cout << " DBG :: UNEXPECTED STATE " << endl;
 
          }
@@ -330,31 +357,31 @@ class CacheProject: public Cache
         for (i = 0; i < ports; i++) { 
 
              if(in_ena[i].read() && in_needs_data[i].read())    {
-		 state.is_read[i]   = true;
-		 state.is_write[i]  = false;
-		 state.is_insert[i] = false;
+		 state_var.is_read[i]   = true;
+		 state_var.is_write[i]  = false;
+		 state_var.is_insert[i] = false;
 		 port_available[i] = false;
 		 delay_cycle[i] = 0;
 	         out_ready[i].write(false); 	 
-		 state.next_state[portno] = INIT;
+		 state_var.next_state[portno] = INIT;
 	     }
 	     else if (in_ena[i].read() && in_has_data[i].read())    {	
-		 state.is_read[i]   = false;
-		 state.is_write[i]  = true;
-		 state.is_insert[i] = false;
+		 state_var.is_read[i]   = false;
+		 state_var.is_write[i]  = true;
+		 state_var.is_insert[i] = false;
 		 port_available[i] = false;
 		 delay_cycle[i] = 0;
 	         out_ready[i].write(false); 	 
-		 state.next_state[i] = INIT;
+		 state_var.next_state[i] = INIT;
 	     }
 	     else if (in_ena[i].read() && in_is_insert[i].read())    {	
-		 state.is_read[i]   = false;
-		 state.is_write[i]  = false;
-		 state.is_insert[i] = true;
+		 state_var.is_read[i]   = false;
+		 state_var.is_write[i]  = false;
+		 state_var.is_insert[i] = true;
 		 port_available[i] = false;
 		 delay_cycle[i] = 0;
 	         out_ready[i].write(false); 	 
-		 state.next_state[i] = INIT;
+		 state_var.next_state[i] = INIT;
              }
 
 	     if (delay_cycle[i] > 0)
@@ -378,17 +405,20 @@ public:
 		/* Signal Binding */
 	for ( i = 0; i < ports; i++ ) {
 	      tag.ena[i](tag_ena[i]);
-	      tag.data[i](tag_data[i]);
+	      tag.din[i](tag_din[i]);
+	      tag.dout[i](tag_dout[i]);
 	      tag.addr[i](tag_addr[i]);
 	      tag.we[i](tag_we[i]);
 
 	      data.ena[i](data_ena[i]);
-	      data.din[i](data_data[i]);
+	      data.din[i](data_din[i]);
+	      data.dout[i](data_dout[i]);
 	      data.addr[i](data_addr[i]);
 	      data.we[i](data_we[i]);
 
 	      state.ena[i](state_ena[i]);
-	      state.din[i](state_datain[i]);
+	      state.din[i](state_din[i]);
+	      state.dout[i](state_dout[i]);
 	      state.addr[i](state_addr[i]);
 	      state.we[i](state_we[i]);
 	}
